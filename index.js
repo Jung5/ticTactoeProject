8f8d5e1f2a18cdcaf19dce058df69f76ec3fa637
jeongwookNa <johnvincent37@outlook.com>
2022-03-02 13:57:44 -0600

    js rough draft

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let statusDisplay = document.querySelector('.status');
+let gameActive = true;
+let curPlyr = "X";
+let gameStat = 
+["", "", "",
+ "", "", "",
+ "", "", ""];
+
+
+ 
 // board memo
 //  012
 //  345
@@ -14,6 +24,24 @@ const toWin = [
     [2, 4, 6]
 ];
 
+function results(){
+    let win = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCond = toWin[i];
+        let a = gameStat[winCond[0]];
+        let b = gameStat[winCond[1]];
+        let c = gameStat[winCond[2]];
+        let d = gameStat[winCond[3]];
+        let e = gameStat[winCond[4]];
+        let f = gameStat[winCond[5]];
+        let g = gameStat[winCond[6]];
+        let h = gameStat[winCond[7]];
+        let i = gameStat[winCond[8]];
+        if (
+
+        )
+    }
+}
 
 // Things to do
 //   game must be turned on and off
