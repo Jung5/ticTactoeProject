7d3447119235f012d314e978099355a5605b09f7
JeongwookNa <najung936@hotmail.com>
2022-03-21 02:16:41 -0500

    used this tutorial to  study tic tac toe as I type each line following the tutorial.  website: https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn. worked on it for 3~4 days. kept on breaking. I will schedule my office hours.

--- index.js
+++ index.js
@@ -1,121 +1,91 @@
-// setting variables.
-const cells = document.querySelectorAll('.cell');
-const announce = document.querySelector('.status');
-// const restartButton = document.querySelector('.restart');
-const cell = [];
-
-const winMsg = () => `${currentPlayer} has won`;
-const drawMsg = () => `Draw`;
-const currentTurnMsg = () => `${currentPlayer}'s turn`;
-
-announce.innerHTML = currentTurnMsg();
-
-let activeGame = true;
-let gameState = ["", "", "", "", "", "", "", "", ""];
+// setting variables
+let gameOn = true;
 let currentPlayer = "X";
-// let computer = "O";
+let gameState = 
+["", "", "",
+ "", "", "",
+ "", "", ""];
 
+ const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-
-// board memo
-//  [0,1,2]
-//  [3,4,5]
-//  [6,7,8]
-
-// const winPossibility = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6]
-// ];
+const announce = document.querySelector('.status');
 
 function tickCells(tickedCell, tickedCellIndex) {
   gameState[tickedCellIndex] = currentPlayer;
   tickedCell.innerHTML = currentPlayer;
 }
 
+const winMsg = () => `${currentPlayer} has won`;
+const drawMsg = () => `Draw`;
+const currentTurnMsg = () => `${currentPlayer}'s turn`;
+
 function playerTurn() {
-  currentPlayer = currentPlayer === "X" ? "Computer" : "X";
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
   announce.innerHTML = currentTurnMsg();
 }
 
-// to win:
 function resultValidation() {
- let winning = false;
-const toWin = () => {
-     if (cell[0] === currentPlayer && cell[1] === currentPlayer && cell[2] === currentPlayer) {
-      return winning = true;
-    }if (cell[3] === currentPlayer && cell[4] === currentPlayer && cell[5] === currentPlayer) {
-      return winning =  true;
-    }if (cell[6] === currentPlayer && cell[7] === currentPlayer && cell[8] === currentPlayer) {
-      return winning = true;
-    }if (cell[0] === currentPlayer && cell[3] === currentPlayer && cell[6] === currentPlayer) {
-      return winning = true;
-    }if (cell[1] === currentPlayer && cell[4] === currentPlayer && cell[7] === currentPlayer) {
-      return winning = true;
-    }if (cell[2] === currentPlayer && cell[5] === currentPlayer && cell[8] === currentPlayer) {
-      return winning = true;
-    }if (cell[0] === currentPlayer && cell[4] === currentPlayer && cell[8] === currentPlayer) {
-      return winning = true;
-    }if (cell[2] === currentPlayer && cell[4] === currentPlayer && cell[6] === currentPlayer) {
-      return winning = true;
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
     }
-    
-    if (winning) {
-      announce.innerText = winMsg();
-      activeGame = false;
-      return;
-    }
-    
-    // to draw:
-    let roundDraw = !gameState.includes("");
-    if (roundDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
+
+    if (roundWon) {
+        announce.innerHTML = winMsg();
+        gameOn = false;
         return;
     }
 
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        announce.innerHTML = drawMsg();
+        gameOn = false;
+        return;
+    }
 
-
-function tickedCells(tickedCellEvent) {
-  const tickedCell = tickedCellEvent.target;
-  const tickedCellIndex = parseInt(tickedCell.getAttribute('data-cell-index'));
-
-  if (gameState[tickedCellIndex] !== "" || !activeGame) {
-      return;
-  }
-
-  tickCells(tickedCell, tickedCellIndex);
-  resultValidation();
+    playerTurn();
 }
 
-// to Restart:
-function restartGame() {
-  activeGame = true;
-  currentPlayer = "X";
-  gameState = ["", "", "", "", "", "", "", "", ""];
-  announce.innerHTML = currentTurnMsg();
-  document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameOn) {
+        return;
+    }
+
+    tickCells(clickedCell, clickedCellIndex);
+    resultValidation();
 }
 
+announce.innerHTML = currentTurnMsg();
 
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', tickedCells));
-document.querySelector('.restart').addEventListener('click', restartGame);
+function handleRestartGame() {
+    gameOn = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    announce.innerHTML = currentTurnMsg();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
 
-// Things to do
-//   game must be turned on and off
-//   cell should react when clicked
-//   same cell cannot be clicked twice
-//   status display 
-//   reset button
-//   read more about querySelector
-//   players must alternate turn
-//   alert, or foreach = try to use it
-// https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn
-// https://github.com/nemo0/tic-tac-toe/blob/master/script.js
-// https://github.com/javascriptacademy-stash/tic-tac-toe/blob/master/index.js
-// https://github.com/angle943/tic-tac-toe/blob/master/app.js}
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.restart').addEventListener('click', handleRestartGame);
